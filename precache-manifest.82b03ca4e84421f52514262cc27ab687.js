effe562fb0abfd6818399b4fd8d38e9422d92f89
unknown <saadali.sheikh7865@example.com>
2020-04-16 11:12:05 +0500

    Updates

---
+++ precache-manifest.82b03ca4e84421f52514262cc27ab687.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ccfada95435cd5c2e622aa421a19c20",
+    "url": "/Snake-Game/index.html"
+  },
+  {
+    "revision": "0fa6c23328a877031911",
+    "url": "/Snake-Game/static/css/main.4d73e0b5.chunk.css"
+  },
+  {
+    "revision": "ddab8d467d23c9f01257",
+    "url": "/Snake-Game/static/js/2.822418a5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Snake-Game/static/js/2.822418a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0fa6c23328a877031911",
+    "url": "/Snake-Game/static/js/main.e8965451.chunk.js"
+  },
+  {
+    "revision": "7b5f35ce07a7c948f4b0",
+    "url": "/Snake-Game/static/js/runtime-main.3308207b.js"
+  }
+]);
